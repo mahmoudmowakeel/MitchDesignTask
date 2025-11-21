a734bb2ed7a47c68255f0406658cabbd6ca0fb0e
m84388533 <mahmoud.el.wakeel@h-partners.com>
2025-11-21 14:51:03 +0200

    Fix Main Route Issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return (
-    <div>
-      <main></main>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/products");
+  }, [router]);
+
+  return null; 
 }
